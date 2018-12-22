2ad5c3c9240360e6d52012400ef0a7fb8a18fc0f
solcr7 <jacozha@gmx.de>
2018-12-22 11:31:35 +0100

    final commit
    restart game bug fixed
    documentation completed
    
    (I know there are still details to do, but my christmas guests are arriving in an hour...merry christmas!)

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-/*creating a card deck */
+/*creating a card deck with image names */
 
 const deck = ["memory_banane",
     "memory_beeren",
@@ -16,6 +16,7 @@ const deck = ["memory_banane",
 
 let fullDeck = deck.concat(deck);
 
+const gameboard = document.querySelector('#memory_gameboard');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -31,7 +32,7 @@ function shuffle(array) {
     return array;
 }
 
-/*setting a timer*/
+/*setting a timer function*/
 
 let seconds = 0;
 function counting() {
@@ -40,12 +41,10 @@ function counting() {
 }
 let timer = null;
 
-
-/* creating the Gameboard */
+/* filling the gameboard with cards */
 
 function startGame() {
     shuffle(fullDeck);
-    const gameboard = document.querySelector('#memory_gameboard');
     let newHtml = ''
 
     for (const cardName of fullDeck) {
@@ -59,19 +58,19 @@ function startGame() {
 
 startGame();
 
-//create star rating
+/* visualize rating */
 
 let moves = 0;
 
-function starRating(number) {
-    const stars = document.getElementsByClassName('star');
-    for (let i = 0; i < stars.length; i++) {
+function carrotRating(number) {
+    const carrots = document.getElementsByClassName('carrot');
+    for (let i = 0; i < carrots.length; i++) {
 
         if (i < number) {
-            stars[i].classList.add('switch_on');
+            carrots[i].classList.add('switch_on');
         }
         else {
-            stars[i].classList.remove('switch_on');
+            carrots[i].classList.remove('switch_on');
         }
     }
 }
@@ -85,85 +84,95 @@ function incrementMoves() {
 
     if (moves < 38) {
         activeCarrots = 5;
-        starRating(5);
+        carrotRating(5);
     }
 
     else if (moves < 44) {
         activeCarrots = 4;
-        starRating(4);
+        carrotRating(4);
     }
 
     else if (moves < 50) {
         activeCarrots = 3;
-        starRating(3);
+        carrotRating(3);
     }
 
     else if (moves < 56) {
         activeCarrots = 2;
-        starRating(2);
+        carrotRating(2);
     }
 
     else {
         activeCarrots = 1;
-        starRating(1);
+        carrotRating(1);
     }
 
 }
 
 /* flipping and matching cards */
-const eventAllCards = document.querySelectorAll('.memory_card');
-for (memory_card of eventAllCards) {
-    memory_card.addEventListener('click', function (event) {
 
-        if (timer == null) {
-            timer = setInterval(counting, 1000);
-        }
-        const flippedCards = document.querySelectorAll('.flipped');
-        const count = flippedCards.length;
-        if (count < 2) {
-            /*adding sound */
-            const sound = new Audio('Click On-SoundBible.com-1697535117.wav');
-            sound.play();
-            incrementMoves();
-            this.classList.add('flipped');
-        }
-        if (count == 1) {
-            const firstCard = flippedCards[0];
-            const firstCardFront = firstCard.querySelector('.memory_card_front');
-            const secondCardFront = this.querySelector('.memory_card_front');
+const clickFunction = function (event) {
 
-            if (firstCardFront.src == secondCardFront.src) {
-                firstCard.classList.add('matched');
-                this.classList.add('matched');
+    if (!event.target.classList.contains('memory_card_back')) {
+        return
+    }
+
+    const memoryCard = event.target.offsetParent;
+
+    if (timer == null) {
+        timer = setInterval(counting, 1000);
+    }
+
+    const flippedCards = document.querySelectorAll('.flipped');
+    const count = flippedCards.length;
+
+    if (count < 2) {
+        /*adding sound */
+        const sound = new Audio('Click On-SoundBible.com-1697535117.wav');
+        sound.play();
+        incrementMoves();
+        memoryCard.classList.add('flipped');
+    }
+
+    if (count == 1) {
+        const firstCard = flippedCards[0];
+        const firstCardFront = firstCard.querySelector('.memory_card_front');
+        const secondCardFront = memoryCard.querySelector('.memory_card_front');
+
+        if (firstCardFront.src == secondCardFront.src) {
+            firstCard.classList.add('matched');
+            memoryCard.classList.add('matched');
+            firstCard.classList.remove('flipped');
+            memoryCard.classList.remove('flipped');
+            //stop timer when all cards match / game is finished
+            const allCardsMatch = document.querySelectorAll('.matched');
+
+            if (allCardsMatch.length == 24) {
+                clearInterval(timer);
+                const alertBox = document.getElementById('alert_box_wrapper');
+                alertBox.classList.add('active');
+                const alertCarrots = document.getElementById('alert_carrots');
+                const alertSeconds = document.getElementById('alert_seconds');
+                const alertMoves = document.getElementById('alert_moves');
+
+                alertCarrots.textContent = activeCarrots;
+                alertSeconds.textContent = seconds;
+                alertMoves.textContent = moves;
+
+            }
+        }
+
+        else {
+            setTimeout(() => {
                 firstCard.classList.remove('flipped');
-                this.classList.remove('flipped');
-                //stop timer when all cards match / game is finished
-                const allCardsMatch = document.querySelectorAll('.matched');
-                if (allCardsMatch.length == 24) {
-                    clearInterval(timer);
-                    const alertBox = document.getElementById('alert_box_wrapper');
-                    alertBox.classList.add('active');
-                    const alertCarrots = document.getElementById('alert_carrots');
-                    const alertSeconds = document.getElementById('alert_seconds');
-                    const alertMoves = document.getElementById('alert_moves');
+                memoryCard.classList.remove('flipped');
+            }, 1500)
 
-                    alertCarrots.textContent = activeCarrots;
-                    alertSeconds.textContent = seconds;
-                    alertMoves.textContent = moves;
-
-                }
-            }
-
-            else {
-                setTimeout(() => {
-                    firstCard.classList.remove('flipped');
-                    this.classList.remove('flipped');
-                }, 1500)
-
-            }
         }
-    })
+    }
 }
+
+gameboard.addEventListener('click', clickFunction);
 
 /* restarting the game / creating alert box with carrot rating / time / restart*/
 
@@ -179,7 +188,7 @@ function newGame() {
     for (const card of allCardsMatch) {
         card.classList.remove('matched');
     }
-    starRating(5);
+    carrotRating(5);
     document.getElementById('alert_box_wrapper').classList.remove('active');
     startGame();
 }
